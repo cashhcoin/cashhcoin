25ab38627ea353c78021ca68109aa2c80e72ca4f
cashhcoin <cashhcoin@gmail.com>
2016-11-29 23:38:14 +0530

    Update protocol.h

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 17682 :26591;
+    return testnet ? 33813 : 17682;
 }
 
 
